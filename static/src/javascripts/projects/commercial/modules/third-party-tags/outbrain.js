137c60f8d3a4b54d2793d86d34efe90e8f2933f8
GHaberis <george.haberis@theguardian.com>
2017-05-22 14:27:32 +0100

    update check dispatcher module

--- static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
+++ static/src/javascripts/projects/commercial/modules/third-party-tags/outbrain.js
@@ -2,7 +2,7 @@
 import detect from 'lib/detect';
 import checkMediator from 'projects/common/modules/check-mediator';
 import { load } from './outbrain-load';
-import { tracking } from './outbrain-tracking';
+// import { tracking } from './outbrain-tracking';
 
 /*
  Loading Outbrain is dependent on successful return of high relevance component
@@ -10,75 +10,65 @@ import { tracking } from './outbrain-tracking';
  not loading Outbrain. As Outbrain is being partially loaded behind the adblock we can
  make the call instantly when we detect adBlock in use.
 */
-const canLoadInstantly = function() {
-    return detect.adblockInUse.then(
+const canLoadInstantly = () =>
+    detect.adblockInUse.then(
         adblockInUse =>
             !document.getElementById('dfp-ad--merchandising-high') ||
             adblockInUse
     );
-};
 
-const onIsOutbrainNonCompliant = function(outbrainNonCompliant) {
-    if (outbrainNonCompliant) load('nonCompliant');
-    else load();
-    tracking({
-        state: outbrainNonCompliant ? 'nonCompliant' : 'compliant',
-    });
+const onIsOutbrainNonCompliant = outbrainNonCompliant => {
+    if (outbrainNonCompliant) {
+        load('nonCompliant');
+    } else {
+        load();
+    }
+
     return Promise.resolve();
 };
 
-const onIsOutbrainMerchandiseCompliant = function(
-    outbrainMerchandiseCompliant
-) {
+const onIsOutbrainMerchandiseCompliant = outbrainMerchandiseCompliant => {
     if (outbrainMerchandiseCompliant) {
         load('merchandising');
-        tracking({
-            state: 'outbrainMerchandiseCompliant',
-        });
         return Promise.resolve();
     }
+
     return checkMediator
         .waitForCheck('isOutbrainNonCompliant')
         .then(onIsOutbrainNonCompliant);
 };
 
-const onIsOutbrainBlockedByAds = function(outbrainBlockedByAds) {
+const onIsOutbrainBlockedByAds = outbrainBlockedByAds => {
     if (outbrainBlockedByAds) {
-        tracking({
-            state: 'outbrainBlockedByAds',
-        });
         return Promise.resolve();
     }
+
     return checkMediator
         .waitForCheck('isOutbrainMerchandiseCompliant')
         .then(onIsOutbrainMerchandiseCompliant);
 };
 
-const onCanLoadInstantly = function(loadInstantly) {
+const onCanLoadInstantly = loadInstantly => {
     if (loadInstantly) {
         return checkMediator
             .waitForCheck('isOutbrainNonCompliant')
             .then(onIsOutbrainNonCompliant);
     }
+
     return checkMediator
         .waitForCheck('isOutbrainBlockedByAds')
         .then(onIsOutbrainBlockedByAds);
 };
 
-const onIsOutbrainDisabled = function(outbrainDisabled) {
+const onIsOutbrainDisabled = outbrainDisabled => {
     if (outbrainDisabled) {
-        tracking({
-            state: 'outbrainDisabled',
-        });
         return Promise.resolve();
     }
+
     return canLoadInstantly().then(onCanLoadInstantly);
 };
 
-const init = function() {
-    return checkMediator
-        .waitForCheck('isOutbrainDisabled')
-        .then(onIsOutbrainDisabled);
-};
+const init = () =>
+    checkMediator.waitForCheck('isOutbrainDisabled').then(onIsOutbrainDisabled);
 
 export { init };
